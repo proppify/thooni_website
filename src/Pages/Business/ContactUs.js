b6f5017faa8fc98cea40616ee272206dec28628a
Sanjay <sanjay@skybers.com>
2023-11-06 11:19:24 +0530

    Raw contact form

--- src/Pages/Business/ContactUs.js
+++ src/Pages/Business/ContactUs.js
@@ -3,8 +3,26 @@ import '../../Styles/Business.css';
 import { Col, Row } from 'react-bootstrap';
 import ContactUsDone from '../../Assets/ContactUs.svg';
 import PrimaryButton from '../../Components/PrimaryButton';
+import { ValidationError, useForm } from '@formspree/react';
 
 const ContactUs = () => {
+    const [state, handleSubmit] = useForm('mnqkawqk');
+    if (state.succeeded) {
+      return <div className='contactcontainer' id='designersadvice'>
+      <Row style={{ backgroundColor: '#EDF8FF' }}>
+          <Col xs={12} md={6}>
+              <div className="contactimage-container1">
+                  <img src={ContactUsDone} alt="card" className="contact2Img" />
+                  <div className="contactoverlay"></div>
+              </div>
+          </Col>
+          <Col xs={12} md={6}>
+          <div>Thank you for submitting the details, we weill get back to you ASAP!</div>
+              
+          </Col>
+      </Row>
+  </div>;
+    }
     return (
         <div className='contactcontainer' id='designersadvice'>
             <Row style={{ backgroundColor: '#EDF8FF' }}>
@@ -15,6 +33,15 @@ const ContactUs = () => {
                     </div>
                 </Col>
                 <Col xs={12} md={6}>
+                <form onSubmit={handleSubmit}>
+                
+                <input id="name" type="text" name="name" placeholder='Name*' required />
+                <input id="email" type="email" name="email" placeholder='Email*' required />
+                <ValidationError field="email" prefix="Email" errors={state.errors} />
+                <input id="number" type="text" name="number" placeholder='Contact*' required />
+                <textarea id="message" rows="4" cols="50" name="message" placeholder='Message' required></textarea>
+                <button type="submit" disabled={state.submitting}>Submit</button>
+                </form>
                     <div className='contactForm'>
                         <div className='contactinputs' >
                             <input placeholder='Name*' type='text' />
